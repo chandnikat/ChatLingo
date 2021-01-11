7d9ac1298111ed33db3e41299b21807542a13e3c
Chandni Patel <chandnipatel@Chandnis-Mac-mini.local>
2021-01-11 16:57:51 -0500

    updated message file

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -35,30 +35,33 @@ const Message = ({ message, name }) => {
   //     <p className='sentTextUser pl'>{message.name}</p>
   //   </div>
   // );
-  return (
+  return message.name === name ? (
     <List className={classes.messageArea}>
-    <ListItem key="1">
-        <Grid container>
-            <Grid item xs={12}>
-                <ListItemText align="right" primary={"Hey man, What's up ?"}></ListItemText>
+        <ListItem key="1">
+            <Grid container>
+                <Grid item xs={12}>
+                    <ListItemText align="right" primary={"Hey man, What's up ?"}></ListItemText>
+                </Grid>
+                <Grid item xs={12}>
+                    <ListItemText align="right" secondary={`${name}- ${time}`}></ListItemText>
+                </Grid>
             </Grid>
-            <Grid item xs={12}>
-                <ListItemText align="right" secondary={`${name}- ${time}`}></ListItemText>
-            </Grid>
-        </Grid>
-    </ListItem>
-    <ListItem key="2">
-        <Grid container>
-            <Grid item xs={12}>
-                <ListItemText align="left" primary="Hey, Iam Good! What about you ?"></ListItemText>
-            </Grid>
-            <Grid item xs={12}>
-                <ListItemText align="left" secondary="09:31"></ListItemText>
-            </Grid>
-        </Grid>
-    </ListItem>
-</List>
-  )
+        </ListItem>
+    </List>
+    ) : (   
+      <List className={classes.messageArea}>
+        <ListItem key="2">
+          <Grid container>
+              <Grid item xs={12}>
+                  <ListItemText align="left" primary="Hey, Iam Good! What about you ?"></ListItemText>
+              </Grid>
+              <Grid item xs={12}>
+                  <ListItemText align="left" secondary={`${name}- ${time}`}></ListItemText>
+              </Grid>
+          </Grid>
+        </ListItem>
+      </List>
+)
 };
 
 export default Message;
