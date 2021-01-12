b6d22da6120ee098a0328194c37bcf7ae5f48c8f
Logan Thies <logan.thies@icloud.com>
2021-01-11 20:09:43 -0800

    update dependancies for "process?"

--- webpack.config.js
+++ webpack.config.js
@@ -84,9 +84,9 @@ module.exports = {
     }),
     // new CleanWebpackPlugin(),
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
-  }),
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+    }),
   ],
 
   resolve: {
