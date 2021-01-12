84fb908bc06d872a7c495b53dc01c6210b19d0fc
Chandni Patel <chandnipatel@Chandnis-Mac-mini.local>
2021-01-11 22:25:29 -0500

    Clean up dictionary code

--- src/components/Dictionary.jsx
+++ src/components/Dictionary.jsx
@@ -1,8 +1,8 @@
 import React, { useState, Component } from 'react';
 import Axios from 'axios';
-import { Languages } from './language';
 
-function VocabAPI() {
+
+function Dictionary() {
   // React Hooks State (Updating state is async)
   const [vocab, setVocab] = useState('');
   const [search, setSearch] = useState('');
@@ -17,21 +17,6 @@ function VocabAPI() {
     setSearch(e.target.value.replace(/ /gi, '%20'));
   };
 
-  const handleSourceLang = (e) => {
-    setSourceLang(e.target.value);
-  };
-
-  const handleTargetLang = (e) => {
-    setTargetLang(e.target.value);
-  };
-
-  const handleLink = () => {
-    let url = `https://translate.google.com/?sl=${sourceLang}&tl=${targetLang}&text=${search}&op=translate`;
-    if (!vocab == '' && definition !== 'Sorry, we cannot find this word') {
-      window.open(url);
-      handleHistory(vocab);
-    }
-  };
 
   // API Functionality
   const handleSubmitVocab = async (e) => {
@@ -56,20 +41,11 @@ function VocabAPI() {
     console.log('Form Submitted');
   };
 
-  // Vocab History Functionality
-  const handleHistory = (v) => {
-    if (vocabHist.length <= 18) {
-      setVocabHist([' ', v, ...vocabHist]);
-    } else {
-      const vocabHistCopy = vocabHist.slice(0, vocabHist.length - 2);
-      setVocabHist([' ', v, ...vocabHistCopy]);
-    }
-  };
 
   //Render
   return (
     <div className="apiContainer">
-      <div className="tools">Chationary Tools</div>
+      <div className="tools">ClatLingo Tools</div>
       <div className="formContainer">
         <form onSubmit={handleSubmitVocab}>
           <label className="apiTextBox">
@@ -88,56 +64,10 @@ function VocabAPI() {
             <p>Definition</p>
             <div className="definition">{definition}</div>
           </div>
-          <div className="langContainer">
-            <p>
-              <label htmlFor="sl">Translate from</label>
-            </p>
-            <select
-              name="sl"
-              id="sl"
-              className="sl"
-              value={sourceLang}
-              onChange={handleSourceLang}
-            >
-              {Languages.map((language) => (
-                <option key={`l-${language.langId}`} value={language.value}>
-                  {language.language}
-                </option>
-              ))}
-            </select>
-            <p>
-              <label htmlFor="tl">Translate to</label>
-            </p>
-            <select
-              name="tl"
-              id="tl"
-              className="tl"
-              value={targetLang}
-              onChange={handleTargetLang}
-            >
-              {Languages.map((language) => (
-                <option key={`l-${language.langId}`} value={language.value}>
-                  {language.language}
-                </option>
-              ))}
-            </select>
-          </div>
         </form>
-      </div>
-
-      <div className="transContainer">
-        <div className="translation">
-          <button id="transBtn" onClick={handleLink}>
-            Translate Vocab
-          </button>
-        </div>
-      </div>
-      <div className="vocabHistContainer">
-        <p>Search History</p>
-        <div className="vocabHist"> {vocabHist} </div>
       </div>
     </div>
   );
 }
 
-export default VocabAPI;
+export default Dictionary;
