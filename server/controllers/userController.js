985d205a4b9880e5c3a089881b1b49d75af2cd09
Trevor Carr <trevor.a.carr@gmail.com>
2021-01-11 22:59:48 -0500

    move verifyUser logic to authController

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -57,49 +57,6 @@ userController.createUser = async (req, res, next) => {
     });
 };
 
-/**
- * verifyUser - Obtain username and password from the request body, locate
- * the appropriate user in the database, and then authenticate the submitted password
- * against the password stored in the database.
- */
-userController.verifyUser = (req, res, next) => {
-  console.log('req.body => ', req.body);
-  const { user_name, password } = req.body;
-  const verifyUserString = `
-        SELECT *
-        from "public"."Users" 
-        WHERE user_name = '${user_name}'
-      ;`;
-
-  db.query(verifyUserString)
-    .then(async response => {
-      console.log(response);
-      if (!response.rows[0]) {
-        return next({
-          message: { err: 'Username and/or password do not match' },
-        });
-      }
-      const hashedPassword = response.rows[0].password;
-      const isMatch = await bcrypt.compare(password, hashedPassword);
-      if (isMatch) {
-        const { id, user_name } = response.rows[0];
-        res.locals.user = { id, user_name };
-        return next();
-      } else {
-        return next({
-          message: { err: 'Username and/or password do not match' },
-        });
-      }
-    })
-    .catch(err => {
-      return next({
-        log: `userController: Unable to verify user data with verifyUser`,
-        message: {
-          err: `userController.verifyUser: ERROR: ${err}`,
-        },
-      });
-    });
-};
 
 userController.checkUsername = async (req, res, next) => {
   console.log('req.body => ', req.body);
