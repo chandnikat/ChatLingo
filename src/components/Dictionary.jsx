dde5782470630801a980d5058158c0de90501dd6
Trevor Carr <trevor.a.carr@gmail.com>
2021-01-12 22:54:08 -0500

    refactor axios post request with correct headers for dictionary route

--- src/components/Dictionary.jsx
+++ src/components/Dictionary.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import Axios from 'axios';
 import { makeStyles } from '@material-ui/core/styles';
-import {Paper, TextField, Button, Grid, Divider, Typography, List, ListItem} from '@material-ui/core';
+import { Paper, TextField, Button, Grid, Divider, Typography, List, ListItem } from '@material-ui/core';
 
 
-const useStyles = makeStyles(theme =>({
-dictionarySection: {
+const useStyles = makeStyles(theme => ({
+  dictionarySection: {
     width: '100%',
     height: '83vh',
   },
@@ -21,7 +21,7 @@ dictionarySection: {
     fontSize: '20px',
     paddingTop: "18px"
   },
-  partOfSpeech:{
+  partOfSpeech: {
     color: '#3caea3',
     fontWeight: 'bold',
     fontSize: '16px',
@@ -38,48 +38,55 @@ dictionarySection: {
 
 const Dictionary = ({ name, room }) => {
   const classes = useStyles();
-   const [vocab, setVocab] = useState('');
-   const [search, setSearch] = useState('');
-   const [definition, setDefinition] = useState(null);
-   const [partOfSpeech, setPartOfSpeech] = useState(null)
-   let [word, setWord] = useState('');
+  const [vocab, setVocab] = useState('');
+  const [search, setSearch] = useState('');
+  const [definition, setDefinition] = useState(null);
+  const [partOfSpeech, setPartOfSpeech] = useState(null)
+  let [word, setWord] = useState('');
 
   //Capitalizes word:
   word = word.toLowerCase().replace(/\b\w{3,}/g, function (l) {
     return l.charAt(0).toUpperCase() + l.slice(1);
   });
- 
-   // React Hooks Functions
-   const handleVocab = (e) => {
-     setVocab(e.target.value);
-     setSearch(e.target.value.replace(/ /gi, '%20'));
-   };
- 
- 
-   // API Functionality
-   const handleSubmitVocab = async (e) => {
-     e.preventDefault(); //Prevents hot reload upon submit
- 
-     const currSearch = e.target[0].value;
-     const body = { vocab: currSearch};
-     try {
-       console.log('Logged try block for post request');
-       const response = await Axios.post('/dictionary', {
-         header: { 'Content-Type': 'Application/JSON' },
-         body: body,
-       });
-       const definition = JSON.stringify(response.data.definition);
-       const partOfSpeech = JSON.stringify(response.data.partOfSpeech);
-       setPartOfSpeech(partOfSpeech)
-       setDefinition(definition);
-       setWord(currSearch);
-      setVocab("")
-     } catch (err) {
-       console.log(`Catch block, POST error on /dictionary: ${err}`);
-     }
-    
 
-   };
+  // React Hooks Functions
+  const handleVocab = (e) => {
+    setVocab(e.target.value);
+    setSearch(e.target.value.replace(/ /gi, '%20'));
+  };
+
+
+  // API Functionality
+  const handleSubmitVocab = async (e) => {
+    e.preventDefault(); //Prevents hot reload upon submit
+    //add token
+    let token = localStorage.getItem('currentUser');
+    console.log('token', token);
+
+    const currSearch = e.target[0].value;
+    const body = { vocab: currSearch };
+    try {
+      console.log('Logged try block for post request');
+      //add authorization header
+      const response = await Axios.post('/dictionary', {
+        body: body,
+      }, {
+        headers: {
+          'Content-Type': 'Application/JSON', 'Authorization': `${token}`,
+        }
+      });
+      const definition = JSON.stringify(response.data.definition);
+      const partOfSpeech = JSON.stringify(response.data.partOfSpeech);
+      setPartOfSpeech(partOfSpeech)
+      setDefinition(definition);
+      setWord(currSearch);
+      setVocab("")
+    } catch (err) {
+      console.log(`Catch block, POST error on /dictionary: ${err}`);
+    }
+
+
+  };
 
   return (
     <div>
@@ -90,31 +97,31 @@ const Dictionary = ({ name, room }) => {
               <Typography className={classes.titleBox}>Dictionary</Typography>
             </ListItem>
             <Divider />
-            <ListItem style={{paddingTop: "20px"}} alignItems="center">
-       
-            <form className={classes.form} onSubmit={handleSubmitVocab}>
+            <ListItem style={{ paddingTop: "20px" }} alignItems="center">
 
-          
-              <TextField
-              variant="outlined"
-              margin="normal"
-              fullWidth
-              id="dictionary"
-              label="Vocabulary Word"
-              autoFocus
-                type="text"
-                name="vocab"
-                value={vocab}
-                onChange={handleVocab}
-              />
+              <form className={classes.form} onSubmit={handleSubmitVocab}>
+
+
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  fullWidth
+                  id="dictionary"
+                  label="Vocabulary Word"
+                  autoFocus
+                  type="text"
+                  name="vocab"
+                  value={vocab}
+                  onChange={handleVocab}
+                />
                 <Button fullWidth
                   variant="contained"
                   color="primary" style={{ fontWeight: '700' }} type="submit">Define</Button>
-          <Typography className={classes.word}>{word}</Typography>
-          <Typography className={classes.partOfSpeech}>{partOfSpeech}</Typography>
-            <Typography>{definition}</Typography>
-       
-        </form>
+                <Typography className={classes.word}>{word}</Typography>
+                <Typography className={classes.partOfSpeech}>{partOfSpeech}</Typography>
+                <Typography>{definition}</Typography>
+
+              </form>
 
             </ListItem>
           </List>
