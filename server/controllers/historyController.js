e6c3a576eb37af22b594598dbadf8022494c6198
Trevor Carr <trevor.a.carr@gmail.com>
2021-01-15 11:39:49 -0500

    successfully getAll and save definitions, translations, and conversations from/to DB

--- server/controllers/historyController.js
+++ server/controllers/historyController.js
@@ -2,6 +2,8 @@ const db = require('../models/chationaryModel');
 
 const historyController = {};
 
+
+//definitons 
 historyController.getAllDefinitions = (req, res, next) => {
   const user_id = res.locals.user_id;
   const retrieveDefinitionsQuery = `SELECT word,definition,part_of_speech FROM "public"."SavedDefinitions" WHERE user_id = '${user_id}';`;
@@ -13,21 +15,6 @@ historyController.getAllDefinitions = (req, res, next) => {
     .catch(err => {
       return next({
         message: { err: 'Error getting saved definitions from database' },
-      });
-    });
-};
-
-historyController.getAllTranslations = (req, res, next) => {
-  const user_id = res.locals.user_id;
-  const retrieveTranslationsQuery = `SELECT word,language_to,language_from, translation FROM "public"."SavedTranslations" WHERE user_id = '${user_id}';`;
-  db.query(retrieveTranslationsQuery)
-    .then(data => {
-      res.locals.translations = data.rows;
-      return next();
-    })
-    .catch(err => {
-      return next({
-        message: { err: 'Error getting saved translations from database' },
       });
     });
 };
@@ -52,7 +39,25 @@ historyController.saveDefinition = (req, res, next) => {
     });
 };
 
-historyController.deleteDefinition = (req, res, next) => {};
+historyController.deleteDefinition = (req, res, next) => { };
+
+
+//translations
+historyController.getAllTranslations = (req, res, next) => {
+  const user_id = res.locals.user_id;
+  const retrieveTranslationsQuery = `SELECT word,language_to,language_from, translation FROM "public"."SavedTranslations" WHERE user_id = '${user_id}';`;
+  db.query(retrieveTranslationsQuery)
+    .then(data => {
+      res.locals.translations = data.rows;
+      return next();
+    })
+    .catch(err => {
+      return next({
+        message: { err: 'Error getting saved translations from database' },
+      });
+    });
+};
+
 
 historyController.saveTranslation = (req, res, next) => {
   const { vocab, tl, sl, translation } = req.body;
@@ -79,12 +84,117 @@ historyController.deleteTranslation = (req, res, next) => {
 
 };
 
-historyController.saveConversation = (req, res, next) => {
-
+//conversations
+historyController.getAllConversations = (req, res, next) => {
+  const user_id = res.locals.user_id;
+  const retrieveConversationsQuery = `SELECT messages, participants, language FROM "public"."SavedConversations" WHERE user_id = '${user_id}';`;
+  db.query(retrieveConversationsQuery)
+    .then(data => {
+      // console.log('hey');
+      // const jsonData = JSON.parse(data);
+      // console.log('json data', jsonData);
+      res.locals.conversations = data.rows;
+      console.log('res.locals', res.locals.conversations)
+      return next();
+    })
+    .catch(err => {
+      return next({
+        message: { err: 'Error getting saved conversations from database' },
+      });
+    });
 };
+
+historyController.saveConversation = (req, res, next) => {
+  const user_id = res.locals.user_id;
+  const conversation = req.body;
+
+  console.log('conversation in saveConversation', conversation);
+
+  // const dummyData = [{
+  //   id: 'jsdfkjdsf',
+  //   name: 'Admin',
+  //   room: 'English',
+  //   text: 'Welcome to English chatroom'
+  // },
+  // {
+  //   id: 'jsdfkjdsf',
+  //   name: 'Logan',
+  //   room: 'English',
+  //   text: 'Hi'
+  // },
+  // {
+  //   id: 'jsdfkjdsf',
+  //   name: 'Anika',
+  //   room: 'English',
+  //   text: 'Hello'
+  // },
+  // {
+  //   id: 'jsdfkjdsf',
+  //   name: 'Trevor',
+  //   room: 'English',
+  //   text: 'Hey!'
+  // },
+  // {
+  //   id: 'jsdfkjdsf',
+  //   name: 'Anika',
+  //   room: 'English',
+  //   text: 'How are you?'
+  // },
+  // {
+  //   id: 'jsdfkjdsf',
+  //   name: 'Logan',
+  //   room: 'English',
+  //   text: 'Great'
+  // },
+  // {
+  //   id: 'jsdfkjdsf',
+  //   name: 'Trevor',
+  //   room: 'English',
+  //   text: 'Same here'
+  // },
+  // ]
+
+  const messages = [];
+  const participantsSet = new Set();
+  const language = dummyData[0].room
+  let chatroom_id;
+
+  if (language === 'English') chatroom_id = 1;
+  else if (language === 'French') chatroom_id = 2;
+  else if (language === 'Spanish') chatroom_id = 3;
+  else if (language === 'German') chatroom_id = 4;
+
+
+  conversation.forEach(message => {
+    messages.push({
+      message: message.text,
+      author: message.name
+    })
+    if (message.name !== 'Admin') participantsSet.add(message.name);
+  })
+  const participantsArray = Array.from(participantsSet);
+
+  const createConversationQuery = `INSERT INTO "public"."SavedConversations" (chatroom_id, messages, participants, language ,user_id)VALUES (
+    $1,$2,$3,$4,$5) RETURNING * ;`;
+
+  const values = [chatroom_id, messages, participantsArray, language, user_id];
+
+  db.query(createConversationQuery, values)
+    .then(data => {
+      console.log(data);
+      return next();
+    })
+    .catch(err => {
+      return next({
+        message: { err: 'Error adding conversation to database' },
+      });
+    })
+}
+
 
 historyController.deleteConversation = (req, res, next) => {
 
 };
+
 
 module.exports = historyController;
